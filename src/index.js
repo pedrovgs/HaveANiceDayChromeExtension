239a100c50332b82a7f9da6ac108793715887652
pedrovgs <pedrovicente.gomez@gmail.com>
2017-11-16 12:15:01 -0300

    Start using promises and configure the usage of slick to be full width

--- src/index.js
+++ src/index.js
@@ -2,24 +2,24 @@ import "slick-carousel";
 import $ from "jquery";
 import { Page, getSmiles } from "./api/haveANiceDayApiClient";
 
-const serializer = new XMLSerializer(typeof XmlElement);
-
 window.onload = () => {
-  const carousel = initializeCarousel();
-  const page = new Page(1, 10);
-  getSmiles(page).then(page => {
-    console.log(`Get smiles result: ${JSON.stringify(page)}`);
+  const carousel = $(".slick-slider");
+  initializeCarousel(carousel).then(() => {
+    const smiles = [1, 2, 3, 4, 5, 6, 7];
+    setTimeout(showSmiles(carousel, smiles), 1000);
   });
-  const smiles = [1, 2, 3, 4, 5, 6, 7];
-  showSmiles(carousel, smiles);
 };
 
-function initializeCarousel() {
-  $(".slick-slider").slick({
-    draggable: false,
-    arrows: false
+function initializeCarousel(carousel) {
+  return new Promise(resolve => {
+    carousel.on("init", () => {
+      resolve();
+    });
+    carousel.slick({
+      draggable: false,
+      arrows: false
+    });
   });
-  return document.getElementsByClassName("slick-slider")[0];
 }
 
 function showSmiles(carousel, smiles) {
@@ -31,7 +31,7 @@ function showSmiles(carousel, smiles) {
 function showSmile(carousel, smile) {
   const title = smile.toString();
   const photo = "https://nerdist.com/wp-content/uploads/2017/03/PnM2iU.jpg";
-  $(".slick-slider").slick(
+  carousel.slick(
     "slickAdd",
     `<img class='smile' src='${photo}' alt='${title}'/>`
   );
